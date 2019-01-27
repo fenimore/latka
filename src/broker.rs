6967154ae16cf92f1935dfa8efa40c7ae5834476
Fenimore Love <exorable.ludos@gmail.com>
2019-01-27 13:17:03 -0500

    Use Partition Struct

--- src/broker.rs
+++ src/broker.rs
@@ -53,9 +53,9 @@ const PRODUCER_MESSAGE_PREFIX: u8 = 78;
 type Offset = u64;
 
 struct Partition {
-    largest_offset: Arc<Mutex<Offset>>,
-    latest_segment: Arc<Mutex<Offset>>,
-    segments_count: Arc<Mutex<u64>>,
+    largest_offset: Mutex<Offset>,
+    latest_segment: Mutex<Offset>,
+    segments_count: Mutex<usize>,
     topic: String,
     partition: u64,
 }
@@ -108,19 +108,13 @@ fn scan_topic(topic: String) -> io::Result<(Offset, usize)> {
 }
 
 
-fn handle_producer(
-    topic: String,
-    stream: TcpStream,
-    offset: Arc<Mutex<Offset>>,
-    last_seg: Arc<Mutex<Offset>>,
-    seg_count: Arc<Mutex<usize>>,
-) -> Result<(), Error> {
+fn handle_producer(stream: TcpStream, partition: Arc<Partition>) -> Result<(), Error> {
     let mut reader = BufReader::new(stream);
 
     'outer: loop {
         let (seg_name, curr_seg): (String, u64) = {
-            let n = last_seg.lock().unwrap();
-            (log_filename(&topic, *n), *n)
+            let n = partition.latest_segment.lock().unwrap();
+            (log_filename(&partition.topic, *n), *n)
         };
         let segment_file = get_segment(seg_name)?;
         let mut segment = BufWriter::new(segment_file);
@@ -135,14 +129,14 @@ fn handle_producer(
             buffer.clear();
 
             // update segment if it is "filled"
-            let mut off = offset.lock().unwrap();
+            let mut off = partition.largest_offset.lock().unwrap();
             *off += n as u64;
-            let mut last_seg = last_seg.lock().unwrap();
+            let mut last_seg = partition.latest_segment.lock().unwrap();
             if *last_seg > curr_seg {
                 println!("Potential misalignment of offsets here");
                 continue 'outer
             }
-            let mut segment_count = seg_count.lock().unwrap();
+            let mut segment_count = partition.segments_count.lock().unwrap();
             if (*segment_count as u64 * SEGMENT_SIZE) < *off {
                 *segment_count += 1;
                 *last_seg = *off;
@@ -154,14 +148,11 @@ fn handle_producer(
     Ok(())
 }
 
-fn handle_consumer(
-    mut stream: TcpStream,
-    topic: String,
-    _global_offset: Arc<Mutex<Offset>>,
-) ->  Result<Offset, Error> {
+fn handle_consumer(mut stream: TcpStream, partition: Arc<Partition>) ->  Result<Offset, Error> {
     let mut offset: Offset = stream.read_u64::<NetworkEndian>()?;
-    println!("Feeding Consumer at Offset: {:?}", offset);
     let mut writer = BufWriter::new(stream);
+
+    println!("Feeding Consumer at Offset: {:?}", offset);
     'infinite: loop{
         // TODO: communicate with consumer to handle disconnection
         // maybe blocking of a BufSream read?
@@ -170,7 +161,7 @@ fn handle_consumer(
         // ATM the consumer is flushed data every segment
         // because the consumer is non-blocking, and the BufWriter
         // isn't configured explicitly, I have to explicitly flush. But where?
-        let sorted_segments = sorted_segments(&topic)?;
+        let sorted_segments = sorted_segments(&partition.topic)?;
         let mut peekable_segments = sorted_segments.iter().peekable();
 
         'outer: loop {
@@ -189,7 +180,7 @@ fn handle_consumer(
                 None => {;}, // reached final segment, so process this segment
             }
             let mut reader: BufReader<File> = {
-                let seg_path = log_filename(&topic, *seg_base_offset);
+                let seg_path = log_filename(&partition.topic, *seg_base_offset);
                 let f = OpenOptions::new().write(false).read(true).open(seg_path)?;
                 let mut reader = BufReader::new(f);
                 let relative_offset = offset - *seg_base_offset;
@@ -270,6 +261,14 @@ fn main() -> Result<(), Error>{
     let last_segment = Arc::new(Mutex::new(largest_base_offset));
     let segment_count = Arc::new(Mutex::new(count));
 
+    let partition = Arc::new(Partition {
+        topic: topic.clone(),
+        partition: 0,
+        segments_count: Mutex::new(count),
+        largest_offset: Mutex::new(largest_base_offset + size_of_last_file),
+        latest_segment: Mutex::new(largest_base_offset),
+    });
+
     for incoming in listener.incoming() {
         let mut stream = match incoming {
             Ok(inc) => inc,
@@ -279,22 +278,18 @@ fn main() -> Result<(), Error>{
         let _ = stream.read(&mut message_type).unwrap();
         match message_type[0] {
             CONSUMER_MESSAGE_PREFIX => {
-                let largest_offset = largest_offset.clone();
-                let t = topic.clone();
+                let partition = Arc::clone(&partition);
                 thread::spawn(|| {
-                    match handle_consumer(stream, t, largest_offset) {
+                    match handle_consumer(stream, partition) {
                         Ok(n) => println!("SUCCESS: Consumer stopped consuming at offset {}", n),
                         Err(e) => println!("ERROR: {:?}", e),
                     };
                 });
             },
             PRODUCER_MESSAGE_PREFIX => {
-                let largest_offset = Arc::clone(&largest_offset);
-                let last_segment = Arc::clone(&last_segment);
-                let segment_count = Arc::clone(&segment_count);
-                let top = topic.clone();
+                let partition = Arc::clone(&partition);
                 thread::spawn(move || {
-                    match handle_producer(top, stream, largest_offset, last_segment, segment_count) {
+                    match handle_producer(stream, partition) {
                         Ok(_) => println!("SUCCESS: Producer finished."),
                         Err(e) => println!("ERROR: {:?}", e),
                     };
